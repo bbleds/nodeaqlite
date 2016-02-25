c0552b62aeaca129095603ac3e90710946ef4114
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-25 13:21:28 -0600

    setting up sql queries

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 'use strict';
 
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./db/chinook.sqlite');
+const db = new sqlite3.Database('./db/Chinook_Sqlite.sqlite');
+
+db.all('SELECT * FROM Employee', (err, res) => {
+  console.log(res);
+});
