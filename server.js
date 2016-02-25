72e1a4d6f35831fd2c529b061cae4c1c524f97e2
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-25 13:30:57 -0600

    set up sql with express

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 'use strict';
 
+const express = require("express");
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./db/Chinook_Sqlite.sqlite');
+const db = new sqlite3.Database('db/Chinook_Sqlite.sqlite');
+const PORT = process.env.PORT || 3000;
 
-db.all('SELECT * FROM Employee', (err, res) => {
-  console.log(res);
+// init app
+const app = express();
+
+console.log('# of invoices per country');
+
+db.all(`
+  SELECT   COUNT(*) AS count,
+           BillingCountry AS country
+  FROM     Invoice
+  GROUP BY BillingCountry
+  ORDER BY count DESC`,
+    (err, res) => {
+      if (err) throw err;
+
+      console.log(res);
+    }
+);
+
+app.get("/", (req, res) =>
+ {
+   res.send("bruh");
+ });
+
+
+app.listen(PORT, () =>
+{
+  console.log(`App listening on port ${PORT}`);
 });
